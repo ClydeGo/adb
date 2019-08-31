f5c5efc2014e4be971de25461512cc91dc90bafd
danielle <danpiamonte@gmail.com>
2019-08-31 15:18:04 +0800

    modified files html

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(function(req,res,next){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
-}));
+});
 
 app.use(express.static(__dirname + '/views/www'));
 console.log(__dirname + '/views/www');
@@ -492,4 +492,4 @@ app.get('/transactionHistory', function(req, res){
 });
 
 
-app.listen(3000);
+app.listen(8080);
