e5cb5060c7fc23a9b92facf05df739dd75a3a6ed
rannzel <rannzel.tongco7@gmail.com>
2019-08-31 14:18:18 +0800

    Added req.body

--- server.js
+++ server.js
@@ -6,11 +6,13 @@ var dbname = "adb";
 var bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:false}));
+
 app.use(function(req,res,next){
 	res.header("Access-Control-Allow-Origin", "*");
 	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 }));
+
 app.use(express.static(__dirname + '/views/www'));
 console.log(__dirname + '/views/www');
 app.set('view engine', 'ejs');
@@ -297,14 +299,14 @@ app.get('/addBalance', function(req, res){
 	});
 });
 
-app.get('/verifyFace', function(req, res){
+app.post('/verifyFace', function(req, res){
 	mongoClient.connect(url, function(err, db){
 		if(err) console.log(err);
 		
 		var database = db.db(dbname);
 
 		collection = database.collection("users");
-		var username = 'clyde';
+		var username = req.body.username;
 
 		collection.find().toArray(function(err, result){
 			if(err) throw err;
